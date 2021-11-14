d5426eb49bcd7e205b86672b4874fa013eb28aed
Tomohiro Kumagai <tomohiro@ez-style.jp>
2021-11-14 14:30:07 +0900

    Initial Commit

---
+++ USBDeviceDetector/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  USBDeviceDetector
+//
+//  Created by Tomohiro Kumagai on 2021/11/14.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
