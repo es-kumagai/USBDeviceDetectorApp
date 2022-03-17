76bdb59aa8f4a6e91a9f9b5f77f612fa3ac350ca
Tomohiro Kumagai <tomohiro@ez-style.jp>
2022-03-17 21:00:00 +0900

    Use USBDeviceDetector framework instead of the implements as a local framework.

---
+++ Trigger/TriggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriggerApp.swift
+//  Trigger
+//
+//  Created by Tomohiro Kumagai on 2022/03/16.
+//
+
+import SwiftUI
+
+@main
+struct TriggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
